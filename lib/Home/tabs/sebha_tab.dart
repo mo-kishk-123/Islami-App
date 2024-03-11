dba5e3800e1b12ea0e6a648f7c8173724aefd47d
mo-kishk-218 <127405347+mo-kishk-218@users.noreply.github.com>
2024-03-11 14:43:26 +0200

    edite in sebha

--- lib/Home/tabs/sebha_tab.dart
+++ lib/Home/tabs/sebha_tab.dart
@@ -8,68 +8,90 @@ class Sebha extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String>azkar =[
+      "سبحان الله",
+      "الحمد الله",
+      "لا اله الا الله",
+      "الله اكبر",
+    ];
+    int index =0;
+    int counter =0;
     return Container(
       width: double.infinity,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SizedBox(
-            height: 105,
+          Expanded(
+            flex: 3,
+              child: 
+              Image.asset("assets/images/sebha.png")
           ),
-          Image.asset("assets/images/sebha.png"),
-          SizedBox(
-            height: 43,
-          ),
-          Container(
-            height: 39,
-            width: 154,
-            child: Text(
-              "عدد التسبيحات",
-              style: GoogleFonts.elMessiri(
-                fontSize: 25,
-                fontWeight: FontWeight.w600,
-                color: Color(0xFF242424),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 26,
-          ),
-          Container(
-            width: 69,
-            height: 81,
-            decoration: BoxDecoration(
-              color: Color(0xFFB7935F),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Center(
-              child: Text("30",
-                  style: GoogleFonts.inter(
-                    color: Color(0xFF242424).withOpacity(.57),
-                    fontWeight: FontWeight.w400,
-                    fontSize: 25,
-                  )),
-            ),
-          ),
-          SizedBox(
-            height: 22,
-          ),
-          Container(
-            width: 137,
-            height: 51,
-            decoration: BoxDecoration(
-              color: Color(0xFFB7935F),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Center(
-              child: Text("سبحان الله",
-                  style: GoogleFonts.inter(
-                    color: Color(0xFFFFFFFF),
-                    fontWeight: FontWeight.w400,
-                    fontSize: 25,
-                  )),
-            ),
-          ),
+
+          Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 43,
+                  ),
+                  Container(
+                    height: 39,
+                    width: 154,
+                    child: Text(
+                      "عدد التسبيحات",
+                      style: GoogleFonts.elMessiri(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF242424),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 26,
+                  ),
+                  Container(
+                    width: 69,
+                    height: 81,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFB7935F),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Center(
+                      child: Text("$counter",
+                          style: GoogleFonts.inter(
+                            color: Color(0xFF242424),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 25,
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 22,
+                  ),
+                  InkWell(
+                    onTap: () {
+
+                    },
+                    child: Container(
+                      width: 137,
+                      height: 51,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFB7935F),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Center(
+                        child: Text("${azkar[index]}",
+                            style: GoogleFonts.inter(
+                              color: Color(0xFFFFFFFF),
+                              fontWeight: FontWeight.w400,
+                              fontSize: 25,
+                            )),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+
         ],
       ),
     );
